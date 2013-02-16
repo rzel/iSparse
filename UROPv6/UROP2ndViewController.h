8eaf9c52f08b861a0ef76366a030c63af5a0346f
scottsievert <sievert.scott@gmail.com>
2013-02-15 19:43:36 -0600

    Before I add the camera roll  stuff.

---
+++ UROPv6/UROP2ndViewController.h
@@ -0,0 +1,16 @@
+//
+//  UROP2ndViewController.h
+//  UROPv6
+//
+//  Created by Scott Sievert on 2/15/13.
+//  Copyright (c) 2013 com.scott. All rights reserved.
+//
+
+
+#import "UROP2ndViewController.h"
+#import "UROPMainViewController.h"
+
+@interface UROP2ndViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
---
+++ UROPv6/UROP2ndViewController.m
@@ -0,0 +1,34 @@
+//
+//  UROP2ndViewController.m
+//  UROPv6
+//
+//  Created by Scott Sievert on 2/15/13.
+//  Copyright (c) 2013 com.scott. All rights reserved.
+//
+
+
+#import "UROP2ndViewController.h"
+#import "UROPMainViewController.h"
+
+@interface UROP2ndViewController ()
+
+@end
+
+@implementation UROP2ndViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    self.imageView.image = [UIImage imageNamed:@"ted.jpg"];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+
+@end
