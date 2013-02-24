23d5747ad6cc96753b8facb38f3abbbec1a584e2
scottsievert <sievert.scott@gmail.com>
2013-02-24 17:49:09 -0600

    Push before push animations again.

--- UROPv6/UROPFinalViewController.m
+++ UROPv6/UROPFinalViewController.m
@@ -184,6 +184,75 @@
 
 }
 
+- (IBAction)reconstructV2:(id)sender {
+    
+    NSLog(@"rate = %f", self.rate);
+    NSLog(@"coarse = %f", self.coarse);
+    
+    // image set up properly
+    self.imageView.image = [self.brain sampleImage:self.imageStay atRate:self.rate];
+    float rate = self.rate;
+    float pix = self.imageStay.size.width * self.imageStay.size.height;
+    float coarseness = self.coarse;
+    NSMutableArray * idx = [[NSMutableArray alloc] initWithCapacity:pix];
+    // init xold
+    float * xold_r = (float *)malloc(sizeof(float) * pix);
+    float * xold_g = (float *)malloc(sizeof(float) * pix);
+    float * xold_b = (float *)malloc(sizeof(float) * pix);
+    float * xold = (float *)malloc(sizeof(float) * 1.01 * pix);
+    
+    float * y_r = (float *)malloc(sizeof(float) * 1.01 * rate * pix);
+    float * y_g = (float *)malloc(sizeof(float) * 1.01 * rate * pix);
+    float * y_b = (float *)malloc(sizeof(float) * 1.01 * rate * pix);
+    float * y = (float *)malloc(sizeof(float) * 1.01 * rate * pix);
+    
+    int i, j;
+    // make measurements
+    // note that idx is modified in this
+    int measurement_length = rate*pix;
+    
+    // make idx
+    // length not needed. [idx count]
+    [self.brain makeIDX:idx ofLength:pix];
+    
+    // make y
+    // length needed
+    // DEBUG: y is only the measurements in one color plane
+    
+    [self.brain makeMeasurements:self.imageStay atRate:rate
+                             red:y_r green:y_g blue:y_b
+                        ofLength:(int)rate*pix
+                             idx:idx];
+    // make xold
+    // length not needed. use pix instead.
+    for (i=0; i<pix; i++) {
+        xold_r[i] = 0;
+        xold_g[i] = 0;
+        xold_b[i] = 0;
+        xold[i] = 0;
+        
+    }
+    UIImage * image =  self.imageView.image;
+    // with change: coarse=0
+    // no change: coarse  = 0.825
+    // an imageView, assumed to be grayed out
+    
+    // image isn't touched in reconstruct2()
+    //
+    // then perhaps somehow presenting the camera roll disables the
+    // animation?
+    
+    // or the animation recieves the same image each time?
+    
+    
+    NSLog(@"self.coarse = %f", self.coarse);
+
+    CABasicAnimation *anim=[CABasicAnimation animationWithKeyPath:@"[self.brain reconstruc2:"];
+    anim.fromValue=[NSNumber numberWithDouble:0.0];
+    anim.toValue=[NSNumber numberWithDouble:M_PI_4];
+    
+
+}
 
 - (void)didReceiveMemoryWarning
 {
