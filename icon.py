820dabfe85258790fdfae5045a60a54daedfdabc
scottsievert <sievert.scott@gmail.com>
2013-07-11 12:16:49 -0500

    Pushing the new icons.

--- icon.py
+++ icon.py
@@ -1,16 +1,19 @@
 
 from pylab import *
 
-n = 57 * 2
-k = zeros((n, n)) + 0.8
+mul = 1
+cmap = 'OrRd'
+
+
+n = 57 * mul
+k = zeros((n, n)) + 0.0
 k[1*n/3:2*n/3, 0*n/3:1*n/3] = 1
 k[1*n/3:2*n/3, 2*n/3:3*n/3] = 1
 k[0*n/3:1*n/3, 1*n/3:2*n/3] = 1
 k[2*n/3:3*n/3, 1*n/3:2*n/3] = 1
 #k = 1-k
 
-filename = 'icon2x.png'
-cmap = 'Blues'
+filename = 'icon2x.png' if mul == 2 else 'icon.png'
 imsave(filename,  k, cmap=cmap)
 imshow(k, cmap=cmap)
 show()
