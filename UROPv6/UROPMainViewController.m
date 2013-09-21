ab1d241d76d6aae35da38f2bbd82d532b18f75b8
scottsievert <sieve121@umn.edu>
2013-09-21 12:18:41 -0500

    Before I screw with the GUI

--- UROPv6/UROPMainViewController.m
+++ UROPv6/UROPMainViewController.m
@@ -82,7 +82,7 @@
     [super viewDidLoad];
 
 	// Do any additional setup after loading the view, typically from a nib.
-    self.text.text = [NSString stringWithFormat:@"Currently, images are sampled at every pixel then information is lost when compressed into a JPG. Why not build a camera that only takes in the information it needs? \n\nThis application will simulate that. It will take an image from your camera roll, then delete some data (without modifing your camera roll). This data is never looked at again, and all of the blacked out areas are not taken in. \n\nThe applications of this are broad. It goes from consumer electronics to medical imaging to discovering planets, and that doesn't even touch an incredible number of fields."];
+    self.text.text = [NSString stringWithFormat:@"Currently, images are sampled at every pixel then information is lost when compressed into a JPG. Why not build a camera that only takes in the information it needs? \n\nThis application will simulate that. It will take an image from your camera roll  and delete some data without modifing your camera roll. From only the pixels shown, an approximation of the original image will be reconstructed. \n\nThe applications of this are broad. It goes from consumer electronics to medical imaging to discovering planets, and that doesn't even touch an incredible number of fields."];
     
     self.text.textColor = [UIColor blackColor];
     
