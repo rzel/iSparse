ee6824b7951f70315f33719baec892eeb16e2d9b
scottsievert <sieve121@umn.edu>
2013-10-19 21:18:11 -0500

    Akshay and I *finally* got it working@!

--- C_imp_xcode/C_imp_xcode/dwt.h
+++
@@ -1,7 +0,0 @@
-
-void * dwt(float * x, int N, float * y);
-float * dwt2(float * x, float * y, int width, int height);
-float * dwt2_full(float * x, int width, int height);
-float * idwt(float * x, int N, float * y);
-float * idwt2(float * x, float * y, int width, int height);
-float * idwt2_full(float * x, int width, int height);
